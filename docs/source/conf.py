488d51ec2b3f45d0d8a8019f834471c10df702ef
Peter Schubert <paschubert@hotmail.com>
2025-04-02 11:29:49 +0200

    models x

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,21 +22,11 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
     'sphinx.ext.mathjax',
     'nbsphinx',
 ]
 
 #mathjax_path = "https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"
-
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),  
-}
-intersphinx_disabled_domains = ['std']
-
-templates_path = ['_templates']
-#latex_engine = 'xelatex'
 
 # -- Options for HTML output
 
@@ -47,8 +37,6 @@ epub_show_urls = 'footnote'
 
 
 # -- Options for LaTeX output --------------------------------------------------
-
-latex_elements = {}
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
